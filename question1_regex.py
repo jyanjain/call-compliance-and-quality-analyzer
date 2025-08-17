1c1f50fdcb7db765a11cceb9a7762aaef01205b4
Jyan Jain <jyanjain252003@gmail.com>
2025-08-18 02:36:28 +0530

    Q1 regex code

--- question1_regex.py
+++ question1_regex.py
@@ -3,21 +3,23 @@ import re
 import json
 
 PROFANITY_PATTERN = re.compile(
-    r"\b("
-    r"fuck|f\*+ck|f\*\*k|f\*\*\*|fuk|fuking|fukin|fucker|fucking|motherfuck(er|a)|mf|"
-    r"shit|shitty|bullshit|b/s|sh!t|sh\*t|"
-    r"ass|a[$]+|arse|asshole|asswipe|assclown|dumbass|jackass|asshat|assface|asslicker|assmuncher|"
-    r"bitch|b[*]+tch|b!tch|biatch|beeotch|sonofabitch|whore|hoe|ho|skank|slut|slag|tramp|"
-    r"bastard|bloody|bollocks|bugger|"
-    r"dick|d[*]+ck|d1ck|dickhead|cock|c0ck|cawk|cocksucker|dildo|blowjob|handjob|wanker|fag|faggot|"
-    r"cunt|kike|spic|chingchong|nigger|nigga|n1gga|beaner|coon|gook|yid|dyke|tranny|shemale|wetback|"
-    r"slut|whore|hoe|ho|skank|tramp|tart|"
-    r"cum|jizz|spunk|jism|orgasm|sex|blowjob|handjob|rimjob|jerk\s?off|wank(er)?|"
-    r"damn|goddamn|godd*mn|hell|christ|jesus\s?christ|"
-    r"crap|screw\s?you|piss\s?off|suck\s?it|eat\s?shit|drop\s?dead|bite\s?me|"
-    r"retard|moron|idiot|stupid|dumb|jerk|loser|sucker|"
-    r"chink|nigger|nigga|spic|kike|wop|gook|raghead|paki|coon|fag|faggot|dyke|queer|tranny"
-    r")\b",
+    r"("
+    r"\b(?:fuck|f\*+ck|f\*\*k|f\*\*\*|fuk|fuking|fukin|fucker|fucking|motherfuck(?:er|a)|mf)\b|"
+    r"\b(?:shit|shitty|bullshit|b/s|sh!t|sh\*t)\b|"
+    r"\b(?:ass|a[$]+|arse|asshole|asswipe|assclown|dumbass|jackass|asshat|assface|asslicker|assmuncher)\b|"
+    r"\b(?:bitch|b[*]+tch|b!tch|biatch|beeotch|sonofabitch|whore|hoe|ho|skank|slut|slag|tramp)\b|"
+    r"\b(?:bastard|bloody|bollocks|bugger)\b|"
+    r"\b(?:damn|dick|d[*]+ck|d1ck|dickhead|cock|c0ck|cawk|cocksucker|dildo|blowjob|handjob|wanker|fag|faggot)\b|"
+    r"\b(?:cunt|kike|spic|chingchong|nigger|nigga|n1gga|beaner|coon|gook|yid|dyke|tranny|shemale|wetback)\b|"
+    r"\b(?:cum|jizz|spunk|jism|orgasm|sex|blowjob|handjob|rimjob)\b|"
+    r"\bjerk\s?off\b|\bwank(?:er)?\b|"
+    r"\b(?:goddamn|godd\*mn|hell|christ)\b|\bjesus\s?christ\b|"
+    r"\bcrap\b|\bscrew\s?you\b|\bpiss\s?off\b|\bsuck\s?it\b|\beat\s?shit\b|\bdrop\s?dead\b|\bbite\s?me\b|"
+    r"\b(?:retard|moron|idiot|stupid|dumb|jerk|loser|sucker)\b|"
+    r"\b(?:chink|wop|raghead|paki|queer)\b|"
+    r"wasting\s+my\s+time|"
+    r"stop\s+wasting\s+my\s+time"
+    r")",
     re.IGNORECASE
 )
 
@@ -54,4 +56,4 @@ def detect_profanity_regex(folder: str):
 
     return agent_profanity_calls, borrower_profanity_calls
 
-# detect_profanity_regex("All_Conversations")
+detect_profanity_regex("All_Conversations")
